c42ccf09ad70d57b8685b78d05116fd2d27a9e75
wildporgHCR2 <121674158+wildporgHCR2@users.noreply.github.com>
2022-12-30 18:54:46 +0000

    small changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-if (window.localStorage.getItem("lang") != "") {
+if (window.localStorage.getItem("lang") != null) {
     console.log("cookies are enabled");
 } else {
-    $("body").append('<div class="cookiepopup">');
-    $(".cookiepopup").append('<p class="cookieheading">')
+    $("body").append('<div class="cookiewindow">');
+    $(".cookiewindow").append('<p class="cookieheading">')
     $(".cookieheading").html("Welcome")
-    $(".cookiepopup").append('<p>')
+    $(".cookiewindow").append('<p class="cookiebody">')
+    $(".cookiebody").html("This site uses cookies.")
 };
